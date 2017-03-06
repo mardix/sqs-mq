07f59af7793093f69b21b461acdd32dea5a4f115
Mardix <mardix@pylot.io>
2017-03-06 04:44:50 -0500

    mysqs TaskQ 1st commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+MySQS
+
+"""
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="mysqs",
+    version="0.0.1",
+    license="MIT",
+    author="Mardix",
+    author_email="mardix@pylot.io",
+    description="",
+    url="",
+    long_description=__doc__,
+    py_modules=['mysqs'],
+    include_package_data=True,
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "boto"
+    ],
+    keywords=['aws'],
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    zip_safe=False
+)
+
