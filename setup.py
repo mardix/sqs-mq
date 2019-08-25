e0aab57225120163d01e1bb8154cb0e3f4f966a0
Mardix <mardix@pylot.io>
2019-08-25 14:42:07 -0400

    Update sqs-mq readme

--- setup.py
+++ setup.py
@@ -18,15 +18,30 @@ A message queue system using AWS SQS
 pip install sqs-mq
 ```
 
-## Create
+## Create/Connect
 
-```
+```python
+
 mq = SQSMQ(name, 
           aws_access_key_id, 
           aws_secret_access_key, 
-          region, visibility_timeout
-          )
+          region, 
+          visibility_timeout)
+
 ```
+
+Alternatively, connect later
+
+```python
+
+mq = SQSMQ(name)
+
+mq.connect(aws_access_key_id, 
+          aws_secret_access_key, 
+          region)
+
+```
+
 
 **SQS-MQ** will create 3 buckets of messages:
 
@@ -36,8 +51,10 @@ mq = SQSMQ(name,
 
 **FAILED**: contains all messages that have failed to process properly, or due to an error.
 
-```
+```python
+
 mq = SQSMQ("myQname", **kwargs)
+
 ```
 
 Three buckets will be created: 
@@ -49,38 +66,48 @@ Three buckets will be created:
 
 ### Add Task
 
+ ```python
 
- ```
  mq.add(my_function_name, *args, **kwargs)
+
  ```
 
 ### Add Task in different pool
 
 To add a task in a different pool. it will be named: `myQname-QUEUED-FAST`
 
-```
+```python
+
 mq.add(my_fn, *args, **kwargs, pool='FAST')
+
 ```
 
 ### Add delay
 
 To add a delay
 
-```
+```python
+
 mq.add(my_fn, delay=45)
+
 ```
 
 ## Run the tasks
 
 ### Run all
 
-```
+```python
+
 mq.run()
+
 ```
 
 ### Run by pool name
-```
+
+```python
+
 mq.run(pool='FAST')
+
 ```
 
 ---
@@ -91,20 +118,24 @@ Once a tasks have been processed, it will be added in either `COMPLETED` or `FAI
 
 ### Run all items in the COMPLETED bucket
 
-```
+```python
+
 def completed_callback(body:dict): 
   data = ...
 
 mq.run_completed(callback=completed_callback)
+
 ```
 
 ### Run all items in the FAILED bucket
 
-```
+```python
+
 def failed_callback(body:dict): 
   data = ...
 
 mq.run_failed(callback=failed_callback)
+
 ```
 
 **Once captured, it will delete the message from the queue**
